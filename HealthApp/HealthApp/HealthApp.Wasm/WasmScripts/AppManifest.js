c07ab3c3bd2eaccd05095d04f3368a0ba2461dfd
Martin Zikmund <martin@zikmund.dev>
2020-08-04 12:31:16 +0200

    Design updates

--- HealthApp/HealthApp/HealthApp.Wasm/WasmScripts/AppManifest.js
+++ HealthApp/HealthApp/HealthApp.Wasm/WasmScripts/AppManifest.js
@@ -1,7 +1,7 @@
 ﻿var UnoAppManifest = {
 
     splashScreenImage: "Assets/SplashScreen.png",
-    splashScreenColor: "#0078D7",
-    displayName: "HealthApp"
+    splashScreenColor: "#333333",
+    displayName: "Uno Health"
 
 }
